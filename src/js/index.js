7d4bd9f30193215dd87ab42e50db6a1226d53aa9
Polina <bad.black.neko.xx@gmail.com>
2022-08-12 20:16:26 +0300

    HTML, CSS fix + part of JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
+const translitBtn = document.querySelector('.translit-text-btn');
+const translitTextInput = document.querySelector('.text-to-translit-input');
+const textToTranslitBox = document.querySelector('.text-to-translit-container');
+const translitText = document.querySelector('.translited-text-container');
 
+translitBtn.addEventListener('click', transliteTextFunc);
